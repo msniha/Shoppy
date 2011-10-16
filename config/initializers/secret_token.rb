2df4844ef31abad0fbe4d5e8cbc08ae458ec8faf
Oussama Issaoui <subroot1986@gmail.com>
2011-10-16 22:43:38 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoppy::Application.config.secret_token = '3ef2d91202910a5b61ca6b91c9f31116203e44b4b3f1438e01a3489dfebf2100643c8b97de7f8cac1f0407c833048053834fb246e6cf324b62e428b69663b111'
