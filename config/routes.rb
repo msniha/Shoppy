5a49d728f091873eae054466c905d78a7f3357f1
Oussama Issaoui <subroot1986@gmail.com>
2011-10-17 14:56:38 +0100

    Done with sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Shoppy::Application.routes.draw do
+
 	resources :users
+	resources :sessions, :only => [:new, :create, :destroy]
+	
+	match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +56,7 @@ Shoppy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "sessions#new"
 
   # See how all your routes lay out with "rake routes"
 
